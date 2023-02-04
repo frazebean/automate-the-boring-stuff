0f952196261dfcbee35ebd1b8d7f1d884af81462
frazebean <muadzshakir26@gmail.com>
2023-02-05 01:57:57 +0800

    Update strong_password_detection.py

--- Chapter 7/strong_password_detection.py
+++ Chapter 7/strong_password_detection.py
@@ -3,24 +3,33 @@
 
 import re
 
-password_input = input("\nEnter password here: ").strip()
 
-# Defining regular expressions for lower/upper case and digits.
-lower_case_regex = re.compile(r"[a-z]+")
-upper_case_regex = re.compile(r"[A-Z]+")
-digit_regex = re.compile(r"\d+")
+def password_strength(password):
+    # Defining regular expressions for lower/upper case and digits.
+    lower_case_regex = re.compile(r"[a-z]+")
+    upper_case_regex = re.compile(r"[A-Z]+")
+    digit_regex = re.compile(r"\d+")
 
-# Check user input for presence of lower/upper case and digits.
-lower_case_detected = lower_case_regex.search(password_input)
-upper_case_detected = upper_case_regex.search(password_input)
-digit_detected = digit_regex.search(password_input)
+    # Check user input for presence of lower/upper case and digits.
+    lower_case_detected = lower_case_regex.search(password)
+    upper_case_detected = upper_case_regex.search(password)
+    digit_detected = digit_regex.search(password)
 
-# Check if password is at least 8 characters long, has at least 1 upper case,
-# lower case and digit.
-if len(password_input) < 8:
-    print("Password is weak. Must be at least 8 characters long.")
-elif None in (lower_case_detected, upper_case_detected, digit_detected):
-    print("""Password is weak. Must have at least one lower case, upper case
-and digit each""")
-else:
-    print("Password is strong. Satisfies all requirements.")
+    # Check if password is at least 8 characters long, has at least 1 upper
+    # case, lower case and digit.
+    if len(password) < 8:
+        print("Password is weak. Must be at least 8 characters long.")
+    elif None in (lower_case_detected, upper_case_detected, digit_detected):
+        print("""Password is weak. Must have at least one lower case, upper
+case and digit each.""")
+    else:
+        print("Password is strong. Satisfies all requirements.")
+
+
+print("""\nFor a password to be considered strong, it must be at least 8
+characters long. It should also have at least 1 lower case, upper case and
+number.""")
+
+password_input_1 = input("\nEnter your password: ").strip()
+
+password_strength(password_input_1)
