34b640853cb8d3e09cc03f1633a1306ee278b523
Ajmel Muadz <muadzshakir26@gmail.com>
2023-02-05 20:58:27 +0800

    Update regex_strip.py

--- Chapter 7/regex_strip.py
+++ Chapter 7/regex_strip.py
@@ -1,11 +1,18 @@
-# PROJECT: regex_strip.py - A regex version of Python's strip() function.'
+# PROJECT: regex_strip.py - A python regex that functions similarly to the strip() function.
 
 import re
 
 user_input = input("Enter string: ")
 argument_input = input("Enter argument to remove character (Enter to skip): ")
 
+# Regex that finds trailing beginning and ending spaces.
+space_regex = re.compile(r"^\s+|\s+$")
+space_detected = space_regex.search(user_input)
 
+# Any trailing space detected will be replaced with "" (no space).
+if space_detected is not None:
+    no_space = user_input.replace(space_detected.group(), "")
+    print(no_space)
 def regex_strip(user_input, argument_input):
     if argument_input == "":
         space_regex = re.compile(r"^\s+|\s+$")
