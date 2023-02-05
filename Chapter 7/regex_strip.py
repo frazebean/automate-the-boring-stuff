31757f66bb2b91b1841d7d4ec04037277530085f
Ajmel Muadz <muadzshakir26@gmail.com>
2023-02-05 19:52:08 +0800

    added comments to program

--- Chapter 7/regex_strip.py
+++ Chapter 7/regex_strip.py
@@ -4,10 +4,11 @@ import re
 
 user_input = input("Enter string: ")
 
-# bspace = beginning_space, espace = ending_space
+# Create a regex that finds trailing beginning and ending spaces.
 space_regex = re.compile(r"^\s+|\s+$")
 space_detected = space_regex.search(user_input)
 
+# Any trailing space detected will be replaced with "" (no space).
 if space_detected is not None:
     no_space = user_input.replace(space_detected.group(), "")
 
