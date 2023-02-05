8ba36c06351eaddfa4f75240ecf971450492976e
Ajmel Muadz <muadzshakir26@gmail.com>
2023-02-05 20:07:26 +0800

    Update regex_strip.py

--- Chapter 7/regex_strip.py
+++ Chapter 7/regex_strip.py
@@ -4,12 +4,11 @@ import re
 
 user_input = input("Enter string: ")
 
-# Create a regex that finds trailing beginning and ending spaces.
+# Regex that finds trailing beginning and ending spaces.
 space_regex = re.compile(r"^\s+|\s+$")
 space_detected = space_regex.search(user_input)
 
 # Any trailing space detected will be replaced with "" (no space).
 if space_detected is not None:
     no_space = user_input.replace(space_detected.group(), "")
-
-print(no_space)
+    print(no_space)
