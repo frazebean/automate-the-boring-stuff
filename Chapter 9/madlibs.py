5f31e482913f0b578511c190ae197b3643e759d8
Ajmel Muadz <muadzshakir26@gmail.com>
2023-02-13 21:51:26 +0800

    Update madlibs.py

--- Chapter 9/madlibs.py
+++ Chapter 9/madlibs.py
@@ -2,23 +2,24 @@
 
 import re
 
-parts_of_speech = ["ADJECTIVE", "NOUN", "ADVERB", "VERB"]
-parts_of_speech_present = []
-
 text_file = open("C:/Users/muadz/Documents/MadLibs.txt")
 text_content = text_file.read()
+modified_text = []
 
-split_text_content = re.split(r'\W', text_content)
+for word in re.split(r"\W", text_content):
+    if "ADJECTIVE" in word:
+        adjective_input = input("Enter an adjective: ").strip()
+        modified_text.append(adjective_input)
+    elif "NOUN" in word:
+        noun_input = input("Enter a noun: ").strip()
+        modified_text.append(noun_input)
+    elif "ADVERB" in word:
+        adverb_input = input("Enter an adverb: ").strip()
+        modified_text.append(adverb_input)
+    elif "VERB" in word:
+        verb_input = input("Enter a verb: ").strip()
+        modified_text.append(verb_input)
+    else:
+        modified_text.append(word)
 
-for word in split_text_content:
-    if word in parts_of_speech:
-        parts_of_speech_present.append(word)
-
-if "ADJECTIVE" in parts_of_speech_present:
-    adjective_input = input("Enter an adjective: ").strip()
-if "NOUN" in parts_of_speech_present:
-    noun_input = input("Enter a noun: ").strip()
-if "ADVERB" in parts_of_speech_present:
-    adverb_input = input("Enter an adverb: ").strip()
-if "VERB" in parts_of_speech_present:
-    verb_input = input("Enter a verb: ").strip()
+print(modified_text)
